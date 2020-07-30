5257f2255e76267033484aa3aa6597630e2d8b7c
Jake VanderPlas <jakevdp@google.com>
2020-07-30 15:23:28 -0700

    fix divide by zero error

--- tests/lax_numpy_test.py
+++ tests/lax_numpy_test.py
@@ -4198,13 +4198,14 @@ def _all_numpy_ufuncs() -> Iterator[str]:
       yield name
 
 
-@jtu.ignore_warning(category=RuntimeWarning, message="divide by zero")
 def _dtypes_for_ufunc(name: str) -> Iterator[Tuple[str, ...]]:
   """Generate valid dtypes of inputs to the given numpy ufunc."""
   func = getattr(np, name)
   for arg_dtypes in itertools.product(*(func.nin * (_all_dtypes,))):
     args = (np.ones(1, dtype=dtype) for dtype in arg_dtypes)
     try:
+      with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", "divide by zero", RuntimeWarning)
         _ = func(*args)
     except TypeError:
       pass
